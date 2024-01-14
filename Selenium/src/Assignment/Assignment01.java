783629e95f2bb67c8eb592c91e18bcd8893fe532
raki9 <raki9@Asus_Rocky>
2024-01-14 11:01:10 +0530

    Changes form lead

--- Selenium/src/Assignment/Assignment01.java
+++ Selenium/src/Assignment/Assignment01.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Assignment01 {
 
 	public static void main(String[] args) throws InterruptedException {
+		//New changes from lead
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
